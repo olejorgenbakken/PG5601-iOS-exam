36ef996c131ec1687ddc6710d6dd816858a83539
Ole Jørgen <olejorgenbakken@icloud.com>
2022-02-26 10:08:38 +0100

    Initial Commit

---
+++ Shared/iOSEksamenApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSEksamenApp.swift
+//  Shared
+//
+//  Created by Ole Jørgen Bakken on 26/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSEksamenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
